2ebb47c4df317150f4dcedc066b9da37db6e0f7f
tarik <63872091+tariksitki@users.noreply.github.com>
2022-04-03 16:14:03 +0200

    project started

---
+++ overview_notes/React_Router/react-router/src/App.js
@@ -0,0 +1,19 @@
+
+
+import "./App.css";
+
+
+
+const App = () => {
+  return (
+    
+    <div className="App">
+      <p>Hello</p>
+    </div>
+  )
+}
+
+export default App
+
+
+// https://github.com/edwardBenedict/router-shop
